75a3bdf16f7f1933e00dfdcb1bd2c5f74511cb51
unknown <you@example.com>
2021-10-26 01:27:11 +0700

    first commit

---
+++ app/src/main/java/com/example/pertemuan3_recyclerview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.pertemuan3_recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addData();
+        recyclerView =
+                (RecyclerView)findViewById(R.id.recycleview);
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+        RecyclerView.LayoutManager layoutManager = new
+                LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+    private void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Rizal", "1918093", "2019"));
+        mahasiswaArrayList.add(new Mahasiswa("Ahmad", "1818093", "2018"));
+        mahasiswaArrayList.add(new Mahasiswa("Fahrezi", "1717093", "2017"));
+        mahasiswaArrayList.add(new Mahasiswa("Zahro", "1616093", "2016"));
+    }
+}
+
+
